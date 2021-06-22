594eae59ddba8caedacbc3eb7dad9d4e7b774548
Lance Hamilton <lance.hamilton24@gmail.com>
2021-06-21 19:37:17 -0500

    Added table to display calculated ratios in the ui

--- FinancialReportsUI/financial-reports-ui-react/src/components/Home/Home.js
+++ FinancialReportsUI/financial-reports-ui-react/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IncomeStatement from '../IncomeStatement/IncomeStatement';
+import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
 
 class Home extends React.Component {
@@ -30,9 +31,9 @@ class Home extends React.Component {
       return (
         <div>
           <h1>Fincancial Statement Analysis</h1>
-          <form onSubmit={this.submitHandler}>
-          <input type="text" placeholder="Enter Company Ticker" ref={this.tickerInput} required></input>
-          <button type="button" onClick={this.submitHandler}>Get Data</button>
+          <form class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Enter Company Ticker" ref={this.tickerInput} required></input>
+          <button type="button" class="btn btn-outline-secondary" onClick={this.submitHandler}>Get Data</button>
           </form>
         </div>
       );
--- FinancialReportsUI/financial-reports-ui-react/src/components/IncomeStatement/IncomeStatement.js
+++ FinancialReportsUI/financial-reports-ui-react/src/components/IncomeStatement/IncomeStatement.js
@@ -20,9 +20,55 @@ class IncomeStatement extends React.Component {
 
   render() {
     this.getAllIncomeStatements(this.props.companyTicker)
+    // const listItems = this.state.incomeStatements.map((incomeStatement) =>
+    //     <li key={incomeStatement.date}>{incomeStatement.year}</li>
+    // );
+    const IncomeStatement = ({ year, symbol, grossprofitratiopercentage, netincomeratiopercentage, sgaratiopercentage, randdatiopercentage, depreciationratiopercentage, interestexpenseratiopercentage, incometaxexpenseratiopercentage }) => (
+      <div>
+        <table>
+            <tr>
+              <th>Year</th>
+              <th>Ticker</th>
+              <th>Profit Margin</th>
+              <th>Net Income Margin</th>
+              <th>SGA Margin</th>
+              <th>R/D Margin</th>
+              <th>Depreciation Margin</th>
+              <th>Interest Expense Margin</th>
+              <th>Income Tax Expense Margin</th>
+            </tr>
+            <tr>
+              <td>{year}</td>
+              <td>{symbol}</td>
+              <td>{grossprofitratiopercentage}%</td>
+              <td>{netincomeratiopercentage}%</td>
+              <td>{sgaratiopercentage}%</td>
+              <td>{randdatiopercentage}%</td>
+              <td>{depreciationratiopercentage}%</td>
+              <td>{interestexpenseratiopercentage}%</td>
+              <td>{incometaxexpenseratiopercentage}%</td>
+            </tr>
+        </table>
+      </div>
+    );
+
     return (
       <div className="container">
         <h4>Company Income Statements</h4>
+        {this.state.incomeStatements.map((incomeStatement) => (
+          <IncomeStatement
+            year={incomeStatement.year}
+            symbol={incomeStatement.symbol}
+            grossprofitratiopercentage={incomeStatement.grossprofitratiopercentage}
+            netincomeratiopercentage={incomeStatement.netincomeratiopercentage}
+            sgaratiopercentag={incomeStatement.sgaratiopercentag}
+            randdatiopercentage={incomeStatement.randdatiopercentage}
+            depreciationratiopercentage={incomeStatement.depreciationratiopercentage}
+            interestexpenseratiopercentage={incomeStatement.interestexpenseratiopercentage}
+            incometaxexpenseratiopercentage={incomeStatement.incometaxexpenseratiopercentage}
+            key={incomeStatement.id}
+          />
+        ))}
       </div>
     );
   }
