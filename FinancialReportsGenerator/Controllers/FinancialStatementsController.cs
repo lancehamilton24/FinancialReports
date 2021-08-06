ad186851e217332893d380a614f62b1e4bf64f3c
Lance Hamilton <lance.hamilton24@gmail.com>
2021-08-06 16:01:26 -0500

    Renamed solutions and project names

---
+++ FinancialReportsGenerator/Controllers/FinancialStatementsController.cs
@@ -0,0 +1,78 @@
+﻿using FinancialReportsApiClient.JsonModels;
+using FinancialReportsApiClient.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FinancialReportsApiClient.Controllers
+{
+    [ApiController]
+    [EnableCors("My Policy")]
+    [Route("api/[controller]")]
+    public class FinancialStatementsController : Controller
+    {
+        [HttpGet("incomestatements/{companyTicker}")]
+        public async Task<List<IncomeStatementJson>> GetAllIncomeStatements(string companyTicker)
+        {
+                List<IncomeStatementJson> incomeStatements;
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://financialmodelingprep.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync($"api/v3/income-statement/{companyTicker}?limit=120&apikey=be1ce41dccee923dcd1484989bc6384b");
+                var incomeStatementJSON = await response.Content.ReadAsStringAsync();
+                incomeStatements = JsonConvert.DeserializeObject<List<IncomeStatementJson>>(incomeStatementJSON);
+                return incomeStatements;
+        }
+
+        [HttpGet("financialstatements/{companyTicker}")]
+        public async Task<FinancialStatement> GetAllFinancialStatements(string companyTicker)
+        {
+            FinancialStatement financialStatements;
+            financialStatements = new FinancialStatement();
+            //financialStatements.CompanyProfile = await GetCompanyProfile(companyTicker);
+            financialStatements.IncomeStatement = await GetAllIncomeStatements(companyTicker);
+            financialStatements.BalanceSheet = await GetAllBalanceSheets(companyTicker);
+            financialStatements.CalculateFinancialSheetRatios();
+            return financialStatements;
+        }
+
+        [HttpGet("balancesheets/{companyTicker}")]
+        public async Task<List<BalanceSheetJson>> GetAllBalanceSheets(string companyTicker)
+        {
+            List<BalanceSheetJson> balanceSheets;
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://financialmodelingprep.com/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //GET Method  
+            HttpResponseMessage response = await client.GetAsync($"api/v3/balance-sheet-statement/{companyTicker}?limit=120&apikey=be1ce41dccee923dcd1484989bc6384b");
+            var balanceSheetJSON = await response.Content.ReadAsStringAsync();
+            balanceSheets = JsonConvert.DeserializeObject<List<BalanceSheetJson>>(balanceSheetJSON);
+            return balanceSheets;
+        }
+
+        [HttpGet("companyprofile/{companyTicker}")]
+         public async Task<List<CompanyProfileJson>> GetCompanyProfile(string companyTicker)
+         {
+             List<CompanyProfileJson> companyProfile;
+             var client = new HttpClient();
+             client.BaseAddress = new Uri("https://financialmodelingprep.com/");
+             client.DefaultRequestHeaders.Accept.Clear();
+             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+             //GET Method  
+             HttpResponseMessage response = await client.GetAsync($"api/v3/quote/{companyTicker}?limit=120&apikey=be1ce41dccee923dcd1484989bc6384b");
+
+             var companyProfileJSON = await response.Content.ReadAsStringAsync();
+            companyProfile = JsonConvert.DeserializeObject<List<CompanyProfileJson>>(companyProfileJSON);
+             return companyProfile;
+         }
+    }
+}
---
+++ FinancialReportsGenerator/Models/FinancialStatement.cs
@@ -0,0 +1,74 @@
+﻿using FinancialReportsApiClient.JsonModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialReportsApiClient.Models
+{
+    public class FinancialStatement
+    {
+        public List<IncomeStatementJson> IncomeStatement { get; set; }
+        public List<BalanceSheetJson> BalanceSheet { get; set; }
+
+        public void CalculateFinancialSheetRatios()
+        {
+            foreach (var bStatement in BalanceSheet)
+            {
+                CalculateNetReceivablesRatio(bStatement);
+                CalculateReturnOnAssetsRatio(bStatement);
+                CalculateReturnOnShareholdersEquityRatio(bStatement);
+            }
+        }
+
+        private void CalculateNetReceivablesRatio(BalanceSheetJson balanceSheet)
+        {
+            try
+            {
+                foreach (var iStatement in IncomeStatement.Where(x => x.Year == balanceSheet.Year))
+                {
+                        double netReceivablesRatio = Math.Round(((double)balanceSheet.NetReceivables / (double)iStatement.Revenue) * 100);
+                        balanceSheet.NetReceivablesRatio = netReceivablesRatio;
+                }
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void CalculateReturnOnAssetsRatio(BalanceSheetJson balanceSheet)
+        {
+            try
+            {
+                foreach (var iStatement in IncomeStatement.Where(x => x.Year == balanceSheet.Year))
+                {
+                    double returnOnAssetsRatio = Math.Round(((double)iStatement.NetIncome / (double)balanceSheet.TotalAssets) * 100);
+                    balanceSheet.ReturnOnAssetsRatio = returnOnAssetsRatio;
+                }
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void CalculateReturnOnShareholdersEquityRatio(BalanceSheetJson balanceSheet)
+        {
+            try
+            {
+                foreach (var iStatement in IncomeStatement.Where(x => x.Year == balanceSheet.Year))
+                {
+                    double returnOnShareholdersEquityRatio = Math.Round(((double)iStatement.NetIncome / (double)balanceSheet.TotalStockholdersEquity) * 100);
+                    balanceSheet.ReturnOnShareholdersEquityRatio = returnOnShareholdersEquityRatio;
+                }
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
